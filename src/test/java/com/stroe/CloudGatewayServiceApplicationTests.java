67332f89813c2d0f7332cb8b077329536b31b09a
Amresh Verma <geniousamresh@gmail.com>
2026-01-11 21:33:25 +0530

    inti

---
+++ src/test/java/com/stroe/CloudGatewayServiceApplicationTests.java
@@ -0,0 +1,14 @@
+/* package com.stroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+ */
