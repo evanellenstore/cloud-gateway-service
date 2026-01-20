ef2bed223beef1e7bf41248d109041933d4ffb05
Amresh Verma <geniousamresh@gmail.com>
2026-01-20 22:53:29 +0530

    updating code

--- src/main/java/com/stroe/config/SecurityConfig.java
+++ src/main/java/com/stroe/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
      @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // your React app origin
+        //config.setAllowedOrigins(List.of("http://localhost:5173")); 
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
